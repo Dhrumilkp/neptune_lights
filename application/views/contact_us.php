71afbdde4342c0aaaa413c7059c3adddd68f1ffa
Dhrumil Patel <48042159+Dhrumilkp@users.noreply.github.com>
2022-01-07 14:50:53 +0530

    Update contact us page

--- application/views/contact_us.php
+++ application/views/contact_us.php
@@ -384,9 +384,29 @@
                                 <div class="row col-mb-50">
                                     <a href="https://www.google.com/maps/place/San+Francisco" target="_blank"
                                         class="col-sm-6">
-                                        <h3 class="h5 mb-2 fw-medium" style="color:black;">San Fransisco</h3>
-                                        <p class="m-0 text-white-50">795 Folsom Ave, Suite 600<br>
-                                            San Francisco, CA 94107</p>
+                                        <h3 class="h5 mb-2 fw-medium" style="color:black;">Ahmedabad</h3>
+                                        <p class="m-0" style="color:black;">
+                                            <?php 
+                                                if(!empty($contact_us_data))
+                                                {
+                                                    foreach($contact_us_data as $row)
+                                                    {
+                                                        echo $row['nep_address'];
+                                                    }
+                                                }  
+                                            ?>
+                                        </p>
+                                        <p class="m-0" style="color:black;margin-top:20px;">
+                                            <?php 
+                                                if(!empty($contact_us_data))
+                                                {
+                                                    foreach($contact_us_data as $row)
+                                                    {
+                                                        echo $row['contact_no'];
+                                                    }
+                                                }  
+                                            ?>
+                                        </p>
                                     </a>
                                 </div>
                             </div>
@@ -396,96 +416,9 @@
                 </div>
             </div>
         </section>
-
-        <footer id="footer" class="border-0 bg-white">
-            <div class="container">
-
-                <div class="footer-widgets-wrap py-lg-6">
-                    <div class="row col-mb-30">
-
-                        <div class="col-lg-2 col-md-3 col-6">
-                            <div class="widget widget_links widget-li-noicon">
-                                <h4 class="ls0 nott">Social</h4>
-                                <ul class="list-unstyled iconlist ms-0">
-                                    <li><a href="https://facebook.com/semicolonweb" target="_blank"><i
-                                                class="icon-facebook"></i> Facebook</a></li>
-                                    <li><a href="https://instagram.com/semicolonweb" target="_blank"><i
-                                                class="icon-instagram"></i> Instagram</a></li>
-                                    <li><a href="https://twitter.com/__semicolon" target="_blank"><i
-                                                class="icon-twitter"></i> Twitter</a></li>
-                                    <li><a href="https://youtube.com/c/SemiColonweb/videos" target="_blank"><i
-                                                class="icon-youtube"></i> YouTube</a></li>
-                                    <li><a href="https://wa.me/00112233344"><i class="icon-whatsapp"></i> WhatsApp</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-2 col-md-3 col-6">
-                            <div class="widget widget_links widget-li-noicon">
-                                <h4 class="ls0 nott">Support</h4>
-                                <ul class="list-unstyled iconlist ms-0">
-                                    <li><a href="demo-furniture.html">Home</a></li>
-                                    <li><a href="demo-furniture-about.html">About</a></li>
-                                    <li><a href="demo-furniture-contact.html">Contact</a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-2 col-md-3 col-6">
-                            <div class="widget widget_links widget-li-noicon">
-                                <h4 class="ls0 nott">Trending</h4>
-                                <ul class="list-unstyled iconlist ms-0">
-                                    <li><a href="demo-furniture-products.html">Shop</a></li>
-                                    <li><a href="demo-forum-single.html">Single</a></li>
-                                    <li><a href="demo-furniture-about.html">Who are we</a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-2 col-md-3 col-6">
-                            <div class="widget widget_links widget-li-noicon">
-                                <h4 class="ls0 nott">Get to Know us</h4>
-                                <ul class="list-unstyled iconlist ms-0">
-                                    <li><a href="intro.html#section-niche">Niche Demos</a></li>
-                                    <li><a href="intro.html#section-multipage">Home Pages</a></li>
-                                    <li><a href="intro.html#section-onepage">One Pages</a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-4">
-                            <div class="widget subscribe-widget clearfix" data-loader="button">
-                                <h4>Subscribe Us</h4>
-                                <h5 class="font-body op-04"><strong>Subscribe</strong> to Our Newsletter to get
-                                    Important News, Amazing Offers &amp; Inside Scoops:</h5>
-                                <div class="widget-subscribe-form-result"></div>
-                                <form id="widget-subscribe-form" action="include/subscribe.php" method="post"
-                                    class="mb-0">
-                                    <div class="input-group">
-                                        <input type="email" id="widget-subscribe-form-email"
-                                            name="widget-subscribe-form-email" class="form-control required email"
-                                            placeholder="Enter your Email Address">
-                                        <button class="btn btn-dark bg-color px-3 input-group-text"
-                                            type="submit">Subscribe</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div id="copyrights" class="py-3 bg-color-light">
-                <div class="container">
-                    <div class="d-flex justify-content-between op-04">
-                        <span>&copy; 2020 All Rights Reserved by Canvas Inc.</span>
-                        <div class="copyright-links"><a href="demo-furniture-contact.html#">Terms of Use</a> / <a
-                                href="demo-furniture-contact.html#">Privacy Policy</a></div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <?php 
+            $this->load->view('footer_landing_view');
+        ?>
     </div>
 
     <div id="gotoTop" class="bg-color op-07 h-op-1">
@@ -499,7 +432,7 @@
         </svg>
     </div>
     <script src="<?php echo base_url(); ?>js/jquery.js"></script>
-    <script src="vjs/plugins.min.js"></script>
+    <script src="<?php echo base_url();?>js/plugins.min.js"></script>
     <script src="<?php echo base_url(); ?>js/functions.js"></script>
 </body>
 
---
+++ application/views/footer_landing_view.php
@@ -0,0 +1,68 @@
+<?php 
+    ?>
+        <footer id="footer" class="border-0 bg-white">
+            <div class="container">
+
+                <div class="footer-widgets-wrap py-lg-6">
+                    <div class="row col-mb-30" id="footer_widget_wrapper" style="display:flex;justify-content:space-between;">
+                        <div class="col-lg-2 col-md-3 col-6">
+                            <div class="widget widget_links widget-li-noicon">
+                                <h4 class="ls0 nott">Social</h4>
+                                <ul class="list-unstyled iconlist ms-0">
+                                    <li><a href="https://facebook.com/semicolonweb" target="_blank"><i
+                                                class="icon-facebook"></i> Facebook</a></li>
+                                    <li><a href="https://instagram.com/semicolonweb" target="_blank"><i
+                                                class="icon-instagram"></i> Instagram</a></li>
+                                    <li><a href="https://twitter.com/__semicolon" target="_blank"><i
+                                                class="icon-twitter"></i> Twitter</a></li>
+                                    <li><a href="https://youtube.com/c/SemiColonweb/videos" target="_blank"><i
+                                                class="icon-youtube"></i> YouTube</a></li>
+                                    <li><a href="https://wa.me/00112233344"><i class="icon-whatsapp"></i> WhatsApp</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-2 col-md-3 col-6">
+                            <div class="widget widget_links widget-li-noicon">
+                                <h4 class="ls0 nott">Support</h4>
+                                <ul class="list-unstyled iconlist ms-0">
+                                    <li><a href="demo-furniture.html">Home</a></li>
+                                    <li><a href="demo-furniture-about.html">About</a></li>
+                                    <li><a href="demo-furniture-contact.html">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- <div class="col-lg-4">
+                            <div class="widget subscribe-widget clearfix" data-loader="button">
+                                <h4>Subscribe Us</h4>
+                                <h5 class="font-body op-04"><strong>Subscribe</strong> to Our Newsletter to get
+                                    Important News, Amazing Offers &amp; Inside Scoops:</h5>
+                                <div class="widget-subscribe-form-result"></div>
+                                <form id="widget-subscribe-form" action="include/subscribe.php" method="post"
+                                    class="mb-0">
+                                    <div class="input-group">
+                                        <input type="email" id="widget-subscribe-form-email"
+                                            name="widget-subscribe-form-email" class="form-control required email"
+                                            placeholder="Enter your Email Address">
+                                        <button class="btn btn-dark bg-color px-3 input-group-text"
+                                            type="submit">Subscribe</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+
+            <div id="copyrights" class="py-3 bg-color-light">
+                <div class="container">
+                    <div class="d-flex justify-content-between op-04">
+                        <span>&copy; 2020 All Rights Reserved by Neptune Lights.</span>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    <?
+?>
